352f3abf765da3299da21a4f9725d1031387c27d
fernando <fernandonetodeveloper@gmail.com>
2023-08-04 16:03:06 -0300

    feat: login page

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,7 +1,44 @@
+import { useState } from "react"
+import "./home.css"
+import { Link } from "react-router-dom"
+
 export default function Home() {
+   const [email, setEmail] = useState("")
+   const [password, setPassword] = useState("")
+
+   function handleLogin(e) {
+      e.preventDefault()
+      if (email !== "" && password !== "") {
+         alert("deu tudo certo " + email)
+      } else {
+         alert("Preencha todos os campos")
+      }
+   }
+
    return (
-      <div>
-         <h1>Página Home</h1>
+      <div className="home-container">
+         <h1>Lista de tarefas</h1>
+         <span>Gerencie sua agenda de forma fácil</span>
+
+         <form className="form" onSubmit={handleLogin}>
+            <input
+               type="text"
+               placeholder="Digite seu e-mail"
+               value={email}
+               onChange={(e) => setEmail(e.target.value)}
+            />
+            <input
+               type="password"
+               autoComplete={false}
+               placeholder="Digite sua senha secreta"
+               value={password}
+               onChange={(e) => setPassword(e.target.value)}
+            />
+            <button type="submit">Fazer login</button>
+         </form>
+         <Link className="button-link" to="/register">
+            Não possui uma conta? Cadastre-se
+         </Link>
       </div>
    )
 }
