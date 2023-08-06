1016584a3e628084e985650aa20636d6dd736b91
fernando <fernandonetodeveloper@gmail.com>
2023-08-06 16:21:46 -0300

    feat: admin page and logout button

--- src/routes/Private.jsx
+++ src/routes/Private.jsx
@@ -20,7 +20,6 @@ export default function Private({ children }) {
                   email: user.email,
                }
                localStorage.setItem("@userDetails", JSON.stringify(userData))
-
                setLoading(false)
                setSigned(true)
             } else {
