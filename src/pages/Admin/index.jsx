25eac49dbdb40c4ef539b19072dee013aa46c5cb
fernando <fernandonetodeveloper@gmail.com>
2023-08-07 14:40:38 -0300

    feat: registering task in the database

--- src/pages/Admin/index.jsx
+++ src/pages/Admin/index.jsx
@@ -1,14 +1,41 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import "./admin.css"
 
-import { auth } from "../../firebaseConnection"
+import { auth, db } from "../../firebaseConnection"
 import { signOut } from "firebase/auth"
+import { addDoc, collection } from "firebase/firestore"
+
 export default function Admin() {
    const [tarefaInput, setTarefaInput] = useState("")
+   const [user, setUser] = useState({})
 
-   function handleRegister(e) {
+   useEffect(() => {
+      async function loadTarefas() {
+         const useDetails = localStorage.getItem("@userDetails")
+         setUser(JSON.parse(useDetails))
+      }
+      loadTarefas()
+   }, [])
+
+   async function handleRegister(e) {
       e.preventDefault()
-      alert("clicou")
+      if (tarefaInput === "") {
+         alert("Digite sua tarefa..")
+         return
+      }
+
+      await addDoc(collection(db, "tarefas"), {
+         tarefa: tarefaInput,
+         created: new Date(),
+         userUid: user?.uid,
+      })
+         .then(() => {
+            console.log("Tarefa registrada")
+            setTarefaInput("")
+         })
+         .catch((error) => {
+            console.log(error)
+         })
    }
 
    async function handleLogout() {
